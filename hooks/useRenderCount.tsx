345ec53e66d2b8476b6cf5abbe6608c7cfaa9b81
ike76 <ike76@me.com>
2021-02-28 05:23:37 -0600

    google analytics

--- hooks/useRenderCount.tsx
+++ hooks/useRenderCount.tsx
@@ -2,5 +2,7 @@ import { useRef } from "react";
 
 export const useRenderCount = (moduleName: string) => {
   const renderCount = useRef(0);
-  console.log(moduleName, "render", renderCount.current++);
+  if (process.env.NODE_ENV === "development") {
+    console.log(moduleName, "render", renderCount.current++);
+  }
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,23 +4,23 @@ import "../styles/globals.css";
 import "../components/BlockContent/prism.css";
 import "../components/BlockContent/highlightPrism.css";
 import DarkNav from "@components/Layout/DarkNav";
-// import { useRouter } from "next/router";
 import { AppProps } from "next/app";
 import { useEffect } from "react";
-import { GA_TRACKING_ID, pageview, event } from "@utils/gtag";
+import * as gtag from "@utils/gtag";
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   useRenderCount("_app");
 
   useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      console.log("handleRouteChange", url);
-      pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
+    if (process.env.NODE_ENV === "production") {
+      const handleRouteChange = (url: URL) => {
+        gtag.pageview(url);
+      };
+      router.events.on("routeChangeComplete", handleRouteChange);
+      return () => {
+        router.events.off("routeChangeComplete", handleRouteChange);
+      };
+    }
   }, [router.events]);
 
   const { layoutInfo } = pageProps;
